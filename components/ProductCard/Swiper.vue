cd31ba426a4541498b2436a71cbfba21359c1912
maximus <leonmax0623@gmail.com>
2023-02-13 07:09:04 +0300

    style:swiper

--- components/ProductCard/Swiper.vue
+++ components/ProductCard/Swiper.vue
@@ -1,19 +1,10 @@
 <script setup>
 	// Import Swiper Vue.js components
 	import { Swiper, SwiperSlide } from 'swiper/vue';
-	// import { useSwiper } from 'swiper/vue';
-	// const sr = useSwiper();
-	// sr.value.slideTo()
+	import { EffectFade } from 'swiper';
 
 	// Import Swiper styles
 	import 'swiper/css';
-
-	import 'swiper/css/thumbs';
-	import 'swiper/css/pagination';
-	import 'swiper/css/navigation';
-
-	// import required modules
-	import { Pagination, Navigation, Thumbs } from 'swiper';
 
 	import img1 from '~/assets/img/product-card/1.png';
 	import img2 from '~/assets/img/product-card/2.png';
@@ -23,6 +14,10 @@
 	import img6 from '~/assets/img/product-card/6.png';
 	import img7 from '~/assets/img/product-card/6.png';
 
+	import bigIm1 from '~/assets/img/promotions/1.jpg';
+	import bigIm2 from '~/assets/img/promotions/2.jpg';
+	import bigIm3 from '~/assets/img/promotions/3.jpg';
+
 	defineProps({
 		premium: {
 			type: Boolean,
@@ -30,12 +25,24 @@
 		},
 	});
 
+	const mainImages = ref([
+		bigIm1,
+		bigIm2,
+		bigIm3,
+		bigIm2,
+		bigIm1,
+		bigIm3,
+		bigIm1,
+	]);
 	const images = ref([img1, img2, img3, img4, img5, img6, img7]);
 
 	// swiper
 
 	const swiperBtn = ref(null);
-	const thumbsSwiper = ref(null);
+
+	const totalImg = computed(() => images.value.length);
+	const currentImg = computed(() => swiperBtn.value?.realIndex + 1);
+
 	function getRef(swiperInstance) {
 		swiperBtn.value = swiperInstance;
 	}
@@ -46,25 +53,25 @@
 	function prev() {
 		swiperBtn.value.slidePrev();
 	}
-	function setThumbsSwiper(swiper) {
-		thumbsSwiper.value = swiper;
-	}
+
 	function slideTo(idx) {
 		swiperBtn.value.slideTo(idx);
 	}
-	const modules = [Pagination, Navigation, Thumbs];
+
+	const modules = [EffectFade];
 </script>
 
 <template>
 	<div class="product-card-swiper">
-		<swiper @swiper="getRef" :modules="modules" class="product-swiper">
-			<swiper-slide v-for="item in 7">
+		<swiper
+			@swiper="getRef"
+			:modules="modules"
+			:effect="'fade'"
+			class="product-swiper"
+		>
+			<swiper-slide v-for="(item, idx) in 7">
 				<div class="product-card-swiper__main">
-					<img
-						class="product-card-swiper__bg"
-						src="~/assets/img/product-card/card-swiper.jpg"
-						alt=""
-					/>
+					<img class="product-card-swiper__bg" :src="mainImages[idx]" alt="" />
 
 					<img
 						src="~/assets/img/product-card/logo.png"
@@ -80,151 +87,35 @@
 				v-if="premium"
 				@click="prev"
 			>
-				<IconArrow class="fill" />
+				<IconArrow :class="{ fill: currentImg != 1 }" />
 			</div>
 			<div
 				class="product-card-swiper__arrow arrow-right pos"
 				@click="next"
 				v-if="premium"
 			>
-				<IconArrow />
+				<IconArrow :class="{ fill: currentImg != totalImg }" />
 			</div>
 			<UISliderButton
-				:current="swiperBtn?.realIndex + 1"
-				:max="swiperBtn?.snapGrid?.length"
+				:current="currentImg"
+				:max="totalImg"
 				class="product-card-swiper__count pos"
 				v-if="premium"
 			/>
 		</swiper>
 
-		<swiper
-			@swiper="setThumbsSwiper"
-			:spaceBetween="5"
-			:watchSlidesProgress="true"
-			:modules="modules"
-			class="product-card-swiper-small"
-		>
-			<swiper-slide v-for="(item, idx) in 7" v-if="premium">
-				<div class="product-card-swiper__thumbnail">
-					<img
-						@click="slideTo(idx)"
-						:src="images[idx]"
-						alt=""
-						:class="{
-							active: idx === swiperBtn?.realIndex,
-						}"
-					/>
-				</div>
-			</swiper-slide>
-		</swiper>
+		<div class="product-card-swiper__thumbnail" v-if="premium">
+			<img
+				v-for="(item, idx) in 7"
+				@click="slideTo(idx)"
+				:src="images[idx]"
+				alt=""
+				:class="{
+					active: idx === swiperBtn?.realIndex,
+				}"
+			/>
+		</div>
 	</div>
 </template>
 
-<style lang="scss">
-	.product-swiper {
-		max-width: 81rem;
-	}
-	.product-card-swiper-small {
-		overflow: visible;
-		& .swiper-slide {
-			max-width: 11rem;
-		}
-	}
-
-	.pos {
-		position: absolute;
-		z-index: 1;
-		top: 0;
-		left: 0;
-	}
-	.product-card-swiper {
-		&__main {
-			position: relative;
-		}
-
-		&__bg {
-			border-radius: 2.8rem;
-			width: 100%;
-			height: 100%;
-			object-fit: cover;
-			max-height: 45rem;
-		}
-
-		&__logo {
-			top: 2rem;
-			right: 2rem;
-			left: auto;
-		}
-
-		&__play {
-			background: rgba($color: #000000, $alpha: 0.4);
-
-			border-radius: 50%;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			padding: 2rem;
-			cursor: pointer;
-		}
-
-		&__arrow {
-			background: $default-color;
-			box-shadow: 1px 4px 12px #a6afcd;
-			border-radius: 50%;
-			padding: 1rem;
-			display: flex;
-			justify-content: center;
-			align-items: center;
-
-			top: 50%;
-			left: 2rem;
-			cursor: pointer;
-			transform: translateY(-50%);
-			& svg {
-				width: 3.5rem;
-				height: 3rem;
-			}
-		}
-
-		&__count {
-			top: auto;
-			bottom: 1.5rem;
-			left: 50%;
-			transform: translateX(-50%);
-		}
-
-		&__thumbnail {
-			max-width: 11rem;
-			display: flex;
-			align-items: center;
-			justify-content: space-between;
-			margin-top: 1.6rem;
-			gap: 1.6rem;
-
-			& img {
-				width: 100%;
-				border: 2px solid transparent;
-				transition: all 0.2s linear;
-				border-radius: 12px;
-				cursor: pointer;
-
-				&.active {
-					border: 2px solid #17a300;
-					filter: drop-shadow(0px 7px 23px rgba(118, 130, 168, 0.55));
-					border-radius: 12px;
-					transform: scale(1.1);
-				}
-			}
-		}
-	}
-	.arrow-left {
-		transform: translateY(-50%) rotate(180deg);
-	}
-	.arrow-right {
-		left: auto;
-		right: 2rem;
-	}
-</style>
+<style lang="scss"></style>
---
+++ layouts/slider.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="wrapper-preview">
+		<slot />
+	</div>
+</template>
+
+<style scoped>
+	.wrapper-preview {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		min-height: 100vh;
+	}
+
+</style>
