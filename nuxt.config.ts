eda7fd240a9d707ae897e009f37610d4d2a0b31c
Vladislav Chistyakov <shirowayfy@gmail.com>
2023-05-31 18:26:16 +0500

    small device zoom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,78 +1,77 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: "Баня.ру – портал о банях и саунах России",
-      link: [],
-    },
-  },
-  modules: ["@vite-pwa/nuxt"],
-  pwa: {
-    registerType: "autoUpdate",
-    manifest: {
-      name: "Баня.ру",
-      short_name: "Баня.ру",
-      description: "Баня.ру – портал о банях и саунах России",
-      theme_color: "#ffffff",
-      icons: [
-        {
-          src: "icons/icon-48x48.png",
-          sizes: "48x48",
-          type: "image/png",
+    app: {
+        head: {
+            title: "Баня.ру – портал о банях и саунах России",
         },
-        {
-          src: "icons/icon-72x72.png",
-          sizes: "72x72",
-          type: "image/png",
-        },
-        {
-          src: "icons/icon-96x96.png",
-          sizes: "96x96",
-          type: "image/png",
-        },
-        {
-          src: "icons/icon-144x144.png",
-          sizes: "144x144",
-          type: "image/png",
-        },
-        {
-          src: "icons/icon-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "icons/icon-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
     },
-    workbox: {
-      navigateFallback: "/",
-    },
-    client: {
-      installPrompt: true,
-    },
-    devOptions: {
-      enabled: true,
-      type: "module",
-    },
-  },
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@use "@/assets/styles/_vars.scss" as *;',
+    modules: ["@vite-pwa/nuxt"],
+    pwa: {
+        registerType: "autoUpdate",
+        manifest: {
+            name: "Баня.ру",
+            short_name: "Баня.ру",
+            description: "Баня.ру – портал о банях и саунах России",
+            theme_color: "#ffffff",
+            icons: [
+                {
+                    src: "icons/icon-48x48.png",
+                    sizes: "48x48",
+                    type: "image/png",
+                },
+                {
+                    src: "icons/icon-72x72.png",
+                    sizes: "72x72",
+                    type: "image/png",
+                },
+                {
+                    src: "icons/icon-96x96.png",
+                    sizes: "96x96",
+                    type: "image/png",
+                },
+                {
+                    src: "icons/icon-144x144.png",
+                    sizes: "144x144",
+                    type: "image/png",
+                },
+                {
+                    src: "icons/icon-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png",
+                },
+                {
+                    src: "icons/icon-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                },
+            ],
         },
-      },
+        workbox: {
+            navigateFallback: "/",
+        },
+        client: {
+            installPrompt: true,
+        },
+        devOptions: {
+            enabled: true,
+            type: "module",
+        },
     },
-    vue: {
-      script: {
-        defineModel: true,
-      },
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@use "@/assets/styles/_vars.scss" as *;',
+                },
+            },
+        },
+        vue: {
+            script: {
+                defineModel: true,
+            },
+        },
     },
-  },
-  css: ["normalize.css/normalize.css", "@/assets/styles/main.scss"],
-  ssr: false,
+    css: ["normalize.css/normalize.css", "@/assets/styles/main.scss"],
+    ssr: false,
 });
